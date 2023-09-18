d225411054e8556504f9d82e3a4364af7c505143
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-09-18 17:07:37 -0600

    g4: using enum to discern settings & add sheets

--- GoodWeather/GoodWeather/GoodWeatherApp.swift
+++ GoodWeather/GoodWeather/GoodWeatherApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct GoodWeatherApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            WeatherListScreen()
         }
     }
 }
--- GoodWeather/GoodWeather/Screens/WeatherListScreen.swift
+++ GoodWeather/GoodWeather/Screens/WeatherListScreen.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+enum Sheets : Identifiable{
+    var id: UUID{
+        return UUID()
+    }
+    case addNewCity
+    case settings
+}
+
 struct WeatherListScreen: View {
     
+    @State private var activeSheet : Sheets?
         
     var body: some View {
         
@@ -16,15 +25,22 @@ struct WeatherListScreen: View {
             ForEach(1...20, id: \.self) { index in
                 Text("\(index)")
             }
-            }
+        }
         .listStyle(PlainListStyle())
-        
+        .sheet(item: $activeSheet, content: { item in
+            switch item{
+                case.addNewCity:
+                    AddCityScreen()
+                case .settings:
+                    SettingsScreen()
+            }
+        })
         .navigationBarItems(leading: Button(action: {
-           
+            activeSheet = .settings
         }) {
             Image(systemName: "gearshape")
         }, trailing: Button(action: {
-            
+            activeSheet = .addNewCity
         }, label: {
             Image(systemName: "plus")
         }))
